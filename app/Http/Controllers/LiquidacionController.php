35023b6c0818f090286bcfbb6b7e9e025c2c6a52
Elias Guere <eliasguere1025@gmail.com>
2020-08-18 11:49:30 -0500

    Completando end point para obtener los finiquitos en formato .csv despendiendo del rango de fechas

--- app/Http/Controllers/LiquidacionController.php
+++ app/Http/Controllers/LiquidacionController.php
@@ -4,23 +4,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\Liquidacion;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Rap2hpoutre\FastExcel\FastExcel;
 
 class LiquidacionController extends Controller
 {
-    public function exportar()
+    public function exportar(Request $request)
     {
-        function finiquitosGenerator() {
+        $desde = Carbon::parse($request->query('desde'));
+        $hasta = Carbon::parse($request->query('hasta'))->lastOfMonth();
+
+        /*
+        return response()->json(
+            Liquidacion::select(
+                'IdLiquidacion',
+                'IdFiniquito',
+                'IdEmpresa',
+                'RutTrabajador',
+                'Mes',
+                'Ano',
+                DB::raw("CAST(ROUND(MontoAPagar, 2, 0) as decimal(18, 2)) MontoAPagar") ,
+                'FechaEmision'
+            )->where('IdEmpresa', 9)->where('IdFiniquito', '<>', '0')
+                ->whereDate('FechaEmision', '>=', $desde)->whereDate('FechaEmision', '<=', $hasta)->get()
+        );*/
+
+        function finiquitosGenerator($desde, $hasta) {
             foreach (
-                Liquidacion::select('IdLiquidacion', 'IdFiniquito', 'IdEmpresa', 'RutTrabajador', 'Mes', 'Ano', 'MontoAPagar')
-                    ->whereIn('IdEmpresa', ['9', '14'])->where('IdFiniquito', '<>', '0')->cursor()
+                Liquidacion::select(
+                    'IdLiquidacion',
+                    'IdFiniquito',
+                    'IdEmpresa',
+                    'RutTrabajador',
+                    'Mes',
+                    'Ano',
+                    DB::raw("CAST(ROUND(MontoAPagar, 2, 0) as decimal(18, 2)) MontoAPagar")
+                )->where('IdEmpresa', 9)->where('IdFiniquito', '<>', '0')
+                    ->whereDate('FechaEmision', '>=', $desde)->whereDate('FechaEmision', '<=', $hasta)->cursor()
                 as $finiquito
             ) {
                 yield $finiquito;
             }
         }
 
-        $finiquitos = finiquitosGenerator();
-        return (new FastExcel($finiquitos))->export('file.csv');
+        $finiquitos = finiquitosGenerator($desde, $hasta);
+
+        return (new FastExcel($finiquitos))->download('finquitos.csv');
     }
 }
