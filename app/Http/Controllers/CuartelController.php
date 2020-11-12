bc6c3240b97f07ca778357b1c8213dfade68f7e9
Elias Guere <eliasguere1025@gmail.com>
2020-11-12 08:19:37 -0500

    Terminando los end points para obtener los datos del formulario

--- app/Http/Controllers/CuartelController.php
+++ app/Http/Controllers/CuartelController.php
@@ -4,9 +4,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cuartel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CuartelController extends Controller
 {
+    public function index($empresaId, Request $request)
+    {
+        $zonaId = $request->query('zonaId');
+        $variedadId = $request->query('variedadId');
+
+        $data = DB::table('dbo.Cuartel as c')
+            ->select(
+                'c.IdCuartel as id',
+                'c.IdZona as zona_labor_id',
+                'c.IdEmpresa as empresa_id',
+                'c.Nombre as name',
+                'c.IdVariedad as variedad_id'
+            )
+            ->where('c.IdEmpresa', $empresaId)
+            ->when($variedadId !== '' && $variedadId !== '0', function($query) use ($variedadId) {
+                $query->where('c.IdVariedad', $variedadId);
+            })
+            ->when($zonaId !== '' && $zonaId != 0, function($query) use ($zonaId) {
+                $query->where('c.IdZona', '=', $zonaId);
+            })
+            ->get();
+
+        return response()->json([
+            'message' => 'Data obtenida',
+            'data' => $data
+        ], 200);
+    }
+
     public function get($id_empresa, $id_zona_labor)
     {
         $data = Cuartel::_get($id_empresa, $id_zona_labor);
--- app/Http/Controllers/LaborController.php
+++ app/Http/Controllers/LaborController.php
@@ -4,9 +4,36 @@
 namespace App\Http\Controllers;
 
 use App\Models\Labor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LaborController extends Controller
 {
+    public function index($empresaId, Request $request)
+    {
+        $unidadMedidaId = $request->query('unidadMedida');
+
+        //dd($unidadMedidaId);
+
+        $data = DB::table('dbo.Actividades as a')
+            ->select(
+                'a.IdActividad as id',
+                'a.IdEmpresa as empresa_id',
+                'a.Nombre as name',
+                'a.UnidadMedida as unidad_medida_id'
+            )
+            ->where('a.IdEmpresa', '=', $empresaId)
+            ->when($unidadMedidaId !== '' && $unidadMedidaId != 0, function($query) use ($unidadMedidaId) {
+                $query->where('a.UnidadMedida', $unidadMedidaId);
+            })
+            ->get();
+
+        return response()->json([
+            'message' => 'Data obtenida',
+            'data' => $data
+        ]);
+    }
+
     public function get($id_empresa, $id_actividad)
     {
         $data = Labor::_get($id_empresa, $id_actividad);
